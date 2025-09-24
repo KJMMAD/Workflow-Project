3a8b830692cfcb6ca70f2190652f227842094c39
cmmsk <cmmsk@KEIRA_LAPTOP>
2025-09-24 13:47:25 -0400

    Add project files.

---
+++ Workflow Project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char first, middle, last;
+	first = 'K';
+	middle = 'J';
+	last = 'M';
+	cout << first << middle << last << endl;
+	return 0;
+}
