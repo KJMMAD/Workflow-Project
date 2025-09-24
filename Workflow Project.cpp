4f9504ed0ebff18b8b92bfa587a1d7d40df28055
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-09-24 14:07:02 -0400

    Changed first and middle initials.

--- Workflow Project.cpp
+++ Workflow Project.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
 	char first, middle, last;
-	first = 'K';
-	middle = 'J';
+	first = 'L';
+	middle = 'C';
 	last = 'M';
 	cout << first << middle << last << endl;
 	return 0;
